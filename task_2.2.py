21728ee98bc9c7bbe39101ff3dd6ef4c5272c6cb
fazliahmetovaNuria <grm716@mail.ru>
2023-06-08 11:11:22 +0300

    the solution to task_2.2

---
+++ task_2.2.py
@@ -0,0 +1,14 @@
+def quarter_of(month):
+     
+    if month == 1 or month == 2 or month == 3:
+        quarter = 1
+    elif month == 4 or month == 5 or month ==6:
+        quarter = 2
+    elif month == 7 or month == 8 or month == 9:
+        quarter = 3
+    elif month ==10 or month == 11 or month == 12:
+        quarter = 4
+
+    return quarter
+
+

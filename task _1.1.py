b02da9a6a2d7d061ce0cea57fa83630496daee89
fazliahmetovaNuria <grm716@mail.ru>
2023-06-08 09:37:24 +0300

    the solution to task_1.1

---
+++ task _1.1.py
@@ -0,0 +1,5 @@
+my_favorite_songs = 'Waste a moment, Staying\' Alive, A Sorta Fairytale, Start Me Up, New Salvation'
+print(my_favorite_songs[:14])
+print(my_favorite_songs[-13:])
+print(my_favorite_songs[16:31])
+print(my_favorite_songs[-26:-15])

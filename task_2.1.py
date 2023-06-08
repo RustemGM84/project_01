f59b45d14f407624d379523dc2f155dbbf8d9a08
fazliahmetovaNuria <grm716@mail.ru>
2023-06-08 10:55:07 +0300

    the solution to task_2.1

---
+++ task_2.1.py
@@ -0,0 +1,15 @@
+def max_num(arr):
+    max = arr[0]
+    for el in arr:
+        if el > max:
+            max = el
+    return max
+
+
+def min_num(arr):
+    min =arr[0]
+    for el in arr:
+        if el < min:
+            min = el
+    return min
+

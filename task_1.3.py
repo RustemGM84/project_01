9c12f22b8f39eafd7af40a41359795bbd8b4f27a
fazliahmetovaNuria <grm716@mail.ru>
2023-06-08 10:35:31 +0300

    the solution to task_1.3

---
+++ task_1.3.py
@@ -0,0 +1,27 @@
+num_month = int(input('Enter the number of month: '))
+if num_month == 1:
+    print('You entered January. 31 days')
+elif num_month == 2:
+    print('You enterd February. 28 days')
+elif num_month == 3:
+    print('You entered March. 31 days')
+elif num_month == 4:
+    print('You entered April. 30 days')
+elif num_month == 5:
+    print('You entered May. 31 days')
+elif num_month == 6:
+    print('You entered June. 30 days')
+elif num_month == 7:
+    print('You entered July. 31 days')
+elif num_month == 8:
+    print('You entered August. 31 days')
+elif num_month ==9:
+    print('You entered September. 30 days')
+elif num_month == 10:
+    print('You entered Oktober. 31 days')
+elif num_month == 11:
+    print('You entered November. 30 days')
+elif num_month == 12:
+    print('You entered December. 31 days')
+else:
+    print('There is no month with this number!')
